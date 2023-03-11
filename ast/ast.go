7749b2b00399a57c6740b56ebb28ea6bfdaca51c
Toby Law <toby@tzcl.me>
2023-03-11 18:58:42 +1100

    Implementing visitor

--- ast/ast.go
+++ ast/ast.go
@@ -2,8 +2,9 @@ package ast
 
 import (
 	"bytes"
-	"github.com/tzcl/monkey/token"
 	"strings"
+
+	"github.com/tzcl/monkey/token"
 )
 
 type Node interface {
@@ -249,4 +250,39 @@ func (ce *CallExpression) String() string {
 	out.WriteString(strings.Join(args, ", "))
 	out.WriteString(")")
 	return out.String()
+}
+
+type ModifierFunc func(Node) Node
+
+func Modify(node Node, modifier ModifierFunc) Node {
+	switch node := node.(type) {
+	case *Program:
+		for i, statement := range node.Statements {
+			node.Statements[i], _ = Modify(statement, modifier).(Statement)
+		}
+
+	case *ExpressionStatement:
+		node.Expression, _ = Modify(node.Expression, modifier).(Expression)
+
+	case *InfixExpression:
+		node.Left, _ = Modify(node.Left, modifier).(Expression)
+		node.Right, _ = Modify(node.Right, modifier).(Expression)
+
+	case *PrefixExpression:
+		node.Right, _ = Modify(node.Right, modifier).(Expression)
+
+	case *IfExpression:
+		node.Condition, _ = Modify(node.Condition, modifier).(Expression)
+		node.Consequence, _ = Modify(node.Consequence, modifier).(*BlockStatement)
+		if node.Alternative != nil {
+			node.Alternative, _ = Modify(node.Alternative, modifier).(*BlockStatement)
+		}
+
+	case *BlockStatement:
+		for i := range node.Statements {
+			node.Statements[i], _ = Modify(node.Statements[i], modifier).(Statement)
+		}
+	}
+
+	return modifier(node)
 }
--- ast/ast_test.go
+++ ast/ast_test.go
@@ -1,8 +1,10 @@
 package ast
 
 import (
-	"github.com/tzcl/monkey/token"
+	"reflect"
 	"testing"
+
+	"github.com/tzcl/monkey/token"
 )
 
 func TestString(t *testing.T) {
@@ -24,5 +26,94 @@ func TestString(t *testing.T) {
 
 	if program.String() != "let myVar = anotherVar;" {
 		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
+func TestModify(t *testing.T) {
+	one := func() Expression { return &IntegerLiteral{Value: 1} }
+	two := func() Expression { return &IntegerLiteral{Value: 2} }
+
+	turnOneIntoTwo := func(node Node) Node {
+		integer, ok := node.(*IntegerLiteral)
+		if !ok {
+			return node
+		}
+
+		if integer.Value != 1 {
+			return node
+		}
+
+		integer.Value = 2
+		return integer
+	}
+
+	tests := []struct {
+		input    Node
+		expected Node
+	}{
+		{
+			one(),
+			two(),
+		},
+		{
+			&Program{
+				Statements: []Statement{
+					&ExpressionStatement{Expression: one()},
+				},
+			},
+			&Program{
+				Statements: []Statement{
+					&ExpressionStatement{Expression: two()},
+				},
+			},
+		},
+		{
+			&InfixExpression{Left: one(), Operator: "+", Right: one()},
+			&InfixExpression{Left: two(), Operator: "+", Right: two()},
+		},
+		{
+			&InfixExpression{Left: two(), Operator: "+", Right: one()},
+			&InfixExpression{Left: two(), Operator: "+", Right: two()},
+		},
+		{
+			&PrefixExpression{Operator: "-", Right: one()},
+			&PrefixExpression{Operator: "-", Right: two()},
+		},
+		{
+			&IfExpression{
+				Condition: one(),
+				Consequence: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: one()},
+					},
+				},
+				Alternative: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: one()},
+					},
+				},
+			},
+			&IfExpression{
+				Condition: two(),
+				Consequence: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: two()},
+					},
+				},
+				Alternative: &BlockStatement{
+					Statements: []Statement{
+						&ExpressionStatement{Expression: two()},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		modified := Modify(tt.input, turnOneIntoTwo)
+
+		equal := reflect.DeepEqual(modified, tt.expected)
+		if !equal {
+			t.Errorf("not equal. got=%#v, want=%#v", modified, tt.expected)
+		}
 	}
 }
